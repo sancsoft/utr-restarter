d2f3c171f44a4e9dc6dfca33179444bc871d3433
amrmahdyy <amrmahdy2@gmail.com>
2024-02-10 14:11:06 -0500

    FEAT: Implemented ILogger interface and LogLevel for structured logging

---
+++ src/ILogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace UTR_Restarter
+{
+	// Defines the severity levels of log messages
+
+	public enum LogLevel
+	{
+		Information,
+		Warning,
+		Error,
+		Critical
+	}
+
+	// Logging service that can log messages at various levels of severity
+
+	public interface ILogger
+	{
+		/// <summary>
+		/// Logs a message with a specific level
+		/// </summary>
+		/// <param name="logLevel">the severity level of message</param>
+		/// <param name="message">the message content</param>
+		/// 
+		void Log(LogLevel logLevel, string message);
+	}
+}
+
